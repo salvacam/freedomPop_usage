0b8da2bba580f93b4792a3d8891fad0717efdfde
Salvador <salvacams@gmail.com>
2017-05-12 09:38:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 let app = {
-  URL_PROXY: 'https://featherbrained-exec.000webhostapp.com/freedomPop_usage/proxy.php',
+  //URL_PROXY: 'https://featherbrained-exec.000webhostapp.com/freedomPop_usage/proxy.php',
+  URL_PROXY: 'https://salvacam.rf.gd/freedomPop_usage/proxy.php',
 
   spinnerDiv: document.getElementById('spinnerDiv'),
   dataDiv: document.getElementById('dataDiv'),
